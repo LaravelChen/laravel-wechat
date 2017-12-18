8cfde225891113755f7a215e294d2473e05534e5
LaravelChen <848407695@qq.com>
2017-12-18 20:38:23 +0800

    it works

---
+++ app/Http/Controllers/WeChat/WeChatController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\WeChat;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class WeChatController extends Controller
+{
+    /**
+     * 处理微信的请求消息
+     *
+     * @return string
+     */
+    public function serve()
+    {
+        $app = app('wechat.official_account');
+        $app->server->push(function ($message) {
+            return "欢迎关注 EasyLaravel！";
+        });
+
+        return $app->server->serve();
+    }
+}
